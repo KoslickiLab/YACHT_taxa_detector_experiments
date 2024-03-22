71ff6d70ab6d910a6649e1a2186e7a36eedaa423
Oliver Bryan <oliverbryan1203@gmail.com>
2024-03-22 13:12:31 -0400

    update create_genome_to_taxid.py

--- create_genome_to_taxid.py
+++ create_genome_to_taxid.py
@@ -1,11 +1,12 @@
 from openpyxl import load_workbook
 from Bio import Entrez
-import csv
 from alive_progress import alive_bar
+import csv
 import time
 
 Entrez.email = 'okb5109@psu.edu'
 file = 'result.xlsx'
+batch_size = 100
 
 print('Reading ' + file)
 workbook = load_workbook(filename=file)
@@ -13,18 +14,30 @@ sheet = workbook.active
 
 output_names = [cell.value for cell in sheet['A']]
 names = [cell.value.split()[0] for cell in sheet['A']]
-batch_size = 500
-stop = len(names)
 
-taxids = []
+name_to_taxid = {}
 
-print(f'Fetching taxids for {stop - 1} genomes')
+print('Reading bacteria_hierarchy.tsv')
+with open('bacteria_hierarchy.tsv', 'r') as ref:
+    reader = csv.reader(ref, delimiter='\t', lineterminator='\n')
+    i = 0
+    for row in reader:
+        if row[3][:3] in ['RS_', 'GB_']:
+            name_to_taxid[row[3][3:]] = row[5]
+
+skipped = []
+for i in range(1, len(names)):
+    if names[i] not in name_to_taxid:
+        skipped.append(names[i])
+print(f'Unable to find {len(skipped)} genomes in bateria_hierarchy.tsv')
+
+print(f'Fetching taxids for {len(skipped)} genomes')
 
 print('Fetching UID numbers')
 id_list = []
-with alive_bar(stop - 1) as bar:
-    for i in range(1, stop, 20):
-        lst = names[i:i + ((stop - i) if (i + 20 > stop) else 20)]
+with alive_bar(len(skipped) - 1) as bar:
+    for i in range(0, len(skipped), 20):
+        lst = skipped[i:i + ((len(skipped) - i) if (i + 20 > len(skipped)) else 20)]
         search = Entrez.esearch(db='assembly', term=' OR '.join(lst))
         for id in Entrez.read(search)['IdList']:
             id_list.append(id)
@@ -42,14 +55,20 @@ with alive_bar(len(id_list)) as bar:
         search = Entrez.efetch(db='assembly', rettype='docsum', retmode='xml', webenv=webenv, query_key=query_key)
         result = Entrez.read(search)
         for j in range(len(lst)):
-            taxids.append(result['DocumentSummarySet']['DocumentSummary'][((len(id_list) %  batch_size) if (i + batch_size >  len(id_list)) else batch_size) - 1 - j]['Taxid'])
+            name_to_taxid[skipped[i + j]] = result['DocumentSummarySet']['DocumentSummary'][((len(id_list) %  batch_size) if (i + batch_size >  len(id_list)) else batch_size) - 1 - j]['Taxid']
             bar()
+
+taxids = []
+for i in range(1, len(names)):
+    try:
+        taxids.append(name_to_taxid[names[i]])
+    except:
+        print(f'Still could not find taxid for genome {names[i]}')
+        exit(-1)
 
 print('Creating genome_to_taxid.tsv')
 with open('genome_to_taxid.tsv', 'w', newline='') as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
     writer.writerow(['genome_id', 'taxid'])
-    with alive_bar(stop - 1) as bar:
-        for i in range(1, stop):
-            writer.writerow([output_names[i], taxids[i - 1]])
-            bar()
+    for i in range(1, len(names)):
+        writer.writerow([output_names[i], taxids[i - 1]])
